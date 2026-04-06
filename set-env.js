11a5f111594c44673bb9ce98fd96c4ae10e3c808
Mangesh Jha <mjha@yotta.com>
2026-04-06 14:45:29 +0530

    changes

--- set-env.js
+++ set-env.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 
 // The Vercel Environment Variables
-const apiKey = process.env.GEMINI_API_KEY || 'AIzaSyBD2GZfnVLClgpD3HhOjtLTr6Wdzqog438';
-const firebaseApiKey = process.env.FIREBASE_API_KEY || 'AIzaSyAsdjyt9vHQ7K7qB4Luiur_KEcBtrla5bs';
-const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN || 'examgenie-bd9e6.firebaseapp.com';
-const firebaseProjectId = process.env.FIREBASE_PROJECT_ID || 'examgenie-bd9e6';
-const firebaseStorageBucket = process.env.FIREBASE_STORAGE_BUCKET || 'examgenie-bd9e6.firebasestorage.app';
-const firebaseMessagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID || '306256796000';
-const firebaseAppId = process.env.FIREBASE_APP_ID || '1:306256796000:web:d12c994e9d1295eec8e605';
-const firebaseMeasurementId = process.env.FIREBASE_MEASUREMENT_ID || 'G-LMBC07F39J';
+const apiKey = process.env.GEMINI_API_KEY;
+const firebaseApiKey = process.env.FIREBASE_API_KEY;
+const firebaseAuthDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const firebaseProjectId = process.env.FIREBASE_PROJECT_ID;
+const firebaseStorageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const firebaseMessagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const firebaseAppId = process.env.FIREBASE_APP_ID;
+const firebaseMeasurementId = process.env.FIREBASE_MEASUREMENT_ID;
 
 const envConfigFile = `
 export const environment = {
@@ -26,9 +26,9 @@ export const environment = {
 };
 `;
 
-console.log('Injecting Vercel Environment Variables into environment.ts...');
+console.log('Injecting Vercel Environment Variables into environment.prod.ts...');
 
-fs.writeFile('./src/environments/environment.ts', envConfigFile, function (err) {
+fs.writeFile('./src/environments/environment.prod.ts', envConfigFile, function (err) {
   if (err) {
     console.log(err);
   } else {
